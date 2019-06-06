338b61a129bbef1c3fe9fdffadea0e7cb07931c8
Demis Balbach <demisbalbach@googlemail.com>
2019-06-06 20:08:45 +0200

    added st config; patched dwm

---
+++ srcpkgs/dwm/files/config.h
@@ -0,0 +1,147 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { 
+						"Terminus (TTF):size=10",
+						"waffle:size=9"	
+				      };
+static const char dmenufont[]    = "Terminus (TTF):size=10";
+
+static const char col_00[]       = "#222222"; 	/* Default Background */
+static const char col_01[]       = "#363537"; 	/* Lighter Background */
+static const char col_02[]       = "#525053"; 	/* Selection Background */
+static const char col_03[]       = "#69676c"; 	/* Comments, Line Highlighting */
+static const char col_04[]       = "#8b888f"; 	/* Dark Foreground */
+static const char col_05[]       = "#f7f1ff"; 	/* Default Foreground */
+static const char col_06[]       = "#"; 	/* Light Forground */
+static const char col_07[]       = "#"; 	/* Light Background */
+static const char col_08[]       = "#"; 	/* Variables, XML-Tags */
+static const char col_09[]       = "#fc618d"; 	/* Red */
+static const char col_0A[]       = "#7bd88f"; 	/* Green */
+static const char col_0B[]       = "#fce566"; 	/* Yellow */
+static const char col_0C[]       = "#fd9353"; 	/* Blue */
+static const char col_0D[]       = "#948ae3"; 	/* Purple */
+static const char col_0E[]       = "#5ad4e6"; 	/* Cyan */
+static const char col_0F[]       = ""; 		/* Accent */
+
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] =	 { col_05, col_00, col_00 },
+	[SchemeSel]  =	 { col_05, col_02, col_02 },
+	[SchemeWarn] =	 { col_0B, col_00, col_0B },
+	[SchemeUrgent]=	 { col_09, col_00, col_09 },
+	[SchemeTSel]=	 { col_04, col_00, col_00 }, /* Selection */
+	[SchemeRed]=	 { col_09, col_00, col_00 }, /* Red */
+	[SchemeGreen]=	 { col_0A, col_00, col_00 }, /* Green */
+	[SchemeYellow]=	 { col_0B, col_00, col_00 }, /* Yellow */
+	[SchemeBlue]=	 { col_0C, col_00, col_00 }, /* Blue */
+	[SchemePurple]=	 { col_0D, col_00, col_00 }, /* Purple */
+	[SchemeCyan]=	 { col_0E, col_00, col_00 }, /* Cyan */
+};
+
+/* tagging */
+static const char *tags[] = { "", "", "", };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod1Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_00, "-nf", col_05, "-sb", col_02, "-sf", col_05, NULL };
+static const char *termcmd[]  = { "st", NULL };
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
+
+
+
+
+
+
+
